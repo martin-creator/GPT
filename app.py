8c259b1147385be9158acf9a5b199c66223c4bd2
martin-creator <martinlubowa@outlook.com>
2023-06-15 18:09:24 +0300

    Basic Completion

---
+++ app.py
@@ -0,0 +1,28 @@
+import os 
+import openai
+
+def init_api():
+    ''' Load API key from .env file'''
+    with open(".env") as env:
+        for line in env:
+            key, value = line.strip().split("=")
+            os.environ[key] = value
+
+    openai.api_key = os.environ["API_KEY"]
+    openai.organization = os.environ["ORG_ID"]
+
+init_api()
+
+next = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Once upon a time",
+    max_tokens=5,
+    temperature=0
+)
+
+print(next)
+
+''' Tokens, by definition, are common sequences of characters in the output text. A good way
+to remember is that one token usually means about 4 letters of text for normal English words. This
+means that 100 tokens are about the same as 75 words. Grasping this will aid you in comprehending
+the pricing. Later in this book, we will delve deeper into pricing details'''
