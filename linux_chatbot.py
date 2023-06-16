7445c0fed87cb4f9c51ac3f9f32734aa2a2543a7
martin-creator <martinlubowa@outlook.com>
2023-06-16 18:26:20 +0300

    Created linux commandline chatbot using openai models

--- linux_chatbot.py
+++ linux_chatbot.py
@@ -1,7 +1,10 @@
 import os
 import openai
+import click
 
 # develop a command-line tool that can assist us with Linux commands through conversation.
+# Click documentation: https://click.palletsprojects.com/en/8.1.x/
+
 
 def init_api():
     ''' Load API key from .env file'''
@@ -16,7 +19,8 @@ def init_api():
 
 init_api()
 
-prompt = """
+# content stuffing the prompt
+_prompt = """
 Input: List all the files in the current directory
 Output: ls -l
 
@@ -33,12 +37,41 @@ Input:{}
 Output:
 """
 
-result = openai.Completion.create(
-    model="text-davinci-002",
-    prompt=prompt.format("Count the number of files in the current directory"),
-    max_tokens=200,
-    temperature=0.0,
-)
 
+while True:
+    request = input(click.style("Input (type 'exit' to quit): ", fg="green"))
+    if request == "exit":
+        break
 
-print(result.choices[0]["text"].strip() )
+    prompt = _prompt.format(request)
+
+    try:
+        result = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=prompt,
+            max_tokens=50,  # Adjust the max_tokens value as needed
+            temperature=0.0,
+        )
+
+        command = result.choices[0].text.strip()
+
+        if command == "":
+            click.echo(click.style("No command generated.", fg="red"))
+            continue
+
+        click.echo( click.style("\nOutput: " + command, fg="yellow"))
+
+        choice = input(click.style("Execute? (y/n): ", fg="green"))
+
+        if choice == "y":
+            os.system(command)
+        elif choice == "n":
+            continue
+        else:
+            click.echo( click.style("Invalid choice. Please enter 'y' or 'n'. ", fg="red"))
+
+    except Exception as e:
+        click.echo( click.style("The command could not be executed. {}".format(e), fg="red"))
+        pass
+
+    click.echo()
