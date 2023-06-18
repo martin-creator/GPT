8326f00cf1dea80f831851df783872f6b1df4273
martin-creator <martinlubowa@outlook.com>
2023-06-18 10:10:00 +0300

    Examples of text embedding in openai using ada

---
+++ semantic_search.py
@@ -0,0 +1,25 @@
+import os
+import openai
+import click
+import pandas as pd
+import numpy as np
+
+# Simple semantic search
+# Click documentation: https://click.palletsprojects.com/en/8.1.x/
+
+
+def init_api():
+    ''' Load API key from .env file'''
+    with open(".env") as env:
+        for line in env:
+            key, value = line.strip().split("=")
+            os.environ[key] = value
+
+    openai.api_key = os.environ["API_KEY"]
+    openai.organization = os.environ["ORG_ID"]
+
+
+init_api()
+
+
+df = pd.read_csv('words.csv')
