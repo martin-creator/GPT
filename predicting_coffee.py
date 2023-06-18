e723ccd72e384b25fa4cf49e148a4c3654698f37
martin-creator <martinlubowa@outlook.com>
2023-06-18 23:14:22 +0300

    Added nltk package to the files

---
+++ predicting_coffee.py
@@ -0,0 +1,6 @@
+# When a user enters the name of a coffee, we will use the OpenAI Embeddings API to get the
+# embedding for the review text of that coffee. Then, we will calculate the cosine similarity between
+# the input coffee review and all other reviews in the dataset. The reviews with the highest cosine
+# similarity scores will be the most similar to the input coffee’s review. We will then print the names
+# of the most similar coffees to the user.
+
--- semantic_search.py
+++ semantic_search.py
@@ -4,6 +4,7 @@ from openai.embeddings_utils import get_embedding,cosine_similarity
 import click
 import pandas as pd
 import numpy as np
+from numpy.linalg import norm
 
 # Simple semantic search
 # Click documentation: https://click.palletsprojects.com/en/8.1.x/
@@ -55,12 +56,51 @@ df["similarity"] = df["embedding"].apply(lambda x: cosine_similarity(x, search_t
 df = df.sort_values(by="similarity", ascending=False)
 
 # print the top 5 results
-print(df.head(5))
+#print(df.head(5))
 
-print(df)
+print(df.head(5))
+print(df.tail(5))
+
+#print(df)
 
 # Detailed description of cosine similarity:
 # https://en.wikipedia.org/wiki/Cosine_similarity
+
+
+# Cosine similarity is a way of measuring how similar two vectors are. It looks at the angle between
+# two vectors (lines) and compares them. Cosine similarity is the cosine of the angle between the
+# vector. A result is a number between -1 and 1. If the vectors are the same, the result is 1. If the
+# vectors are completely different, the result is -1. If the vectors are at a 90-degree angle, the result is
+# 0. In mathematical terms, this is the equation:
+# $$
+# Similarity = (A.B) / (||A||.||B||)
+# $$
+# • A and B are vectors
+# • A.B is a way of multiplying two sets of numbers together. It is done by taking each number
+# in one set and multiplying it with the same number in the other set, then adding all of those
+# products together.
+# • ||A|| is the length of the vector A. It is calculated by taking the square root of the sum of the
+# squares of each element of the vector A.
+# Let’s consider vector A = [2,3,5,2,6,7,9,2,3,4] and vector B = [3,6,3,1,0,9,2,3,4,5].
+# This is how we can get the cosine similarity between them using Python:
+
+
+# define two vectors
+A = np.array([2,3,5,2,6,7,9,2,3,4])
+B = np.array([3,6,3,1,0,9,2,3,4,5])
+
+# calculate the dot product
+dot_product = np.dot(A,B)
+
+# calculate the length of each vector
+A_length = norm(A)
+B_length = norm(B)
+
+# calculate the cosine similarity
+cosine_similarity = dot_product / (A_length * B_length)
+
+print( f"cosine_similarity: { cosine_similarity}")
+
 
 
 
