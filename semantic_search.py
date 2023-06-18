f95935ace469de681aed5f2e3194ec542757d1aa
martin-creator <martinlubowa@outlook.com>
2023-06-18 10:59:02 +0300

    Added logic to calculate cosine similarity between search term and word in csv

--- semantic_search.py
+++ semantic_search.py
@@ -1,6 +1,6 @@
 import os
 import openai
-from openai.embeddings_utils import get_embedding
+from openai.embeddings_utils import get_embedding,cosine_similarity
 import click
 import pandas as pd
 import numpy as np
@@ -30,6 +30,21 @@ df = pd.read_csv('words.csv') # This line creates a pandas dataframe from the cs
 # print(get_embedding("Hello", engine="text-embedding-ada-002"))
 
 # get embedding for each word in the dataframe
-df['embedding'] = df['text'].apply(lambda x: get_embedding(x, engine="text-embedding-ada-002"))
+df['embedding'] = df['Text'].apply(lambda x: get_embedding(x, engine="text-embedding-ada-002"))
 
 df.to_csv('embeddings.csv')
+
+# convert last column to numpy array
+df["embedding"] = df["embedding"].apply(eval).apply(np.array)
+
+# get the search term from the user
+user_search = input("Enter a search term: ")
+
+# get the embedding for the search term
+user_embedding = get_embedding(user_search, engine="text-embedding-ada-002")
+
+# calculate the similarity between the search term and each word in the dataframe
+df["similarity"] = df["embedding"].apply(lambda x: cosine_similarity(x, user_embedding))
+
+
+
