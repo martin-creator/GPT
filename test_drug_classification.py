de849a6090a51c1c7b7d2b7b57732eb71a9a51d4
martin-creator <martinlubowa@outlook.com>
2023-06-21 23:47:18 +0300

    Successfully created text for drug classsification model

--- test_drug_classification.py
+++ test_drug_classification.py
@@ -22,10 +22,17 @@ model = "curie:ft-personal-2023-06-21-19-07-39"
 
 # Let's use a drug from each class
 drugs = [
-    "A CN Gel(Topical) 20gmA CN Soap 75gm",  # Class 0
-    "Addnok Tablet 20'S",  # Class 1
-    "ABICET M Tablet 10's",  # Class
+    "A CN Gel(Topical) 20gmA CN Soap 75gm' used for?",  # Class 0
+    "Addnok Tablet 20'S' used for?",  # Class 1
+    "ABICET M Tablet 10's' used for?",  # Class
 ]
+
+class_map = {
+    0: "Acne",
+    1: "Adhd",
+    2: "Allergies"
+    # ...
+}
 
 # Returns a drug class for each drug
 for drug_name in drugs:
@@ -39,7 +46,13 @@ for drug_name in drugs:
     )
 
     # Print the generated text
-    drug_class = response.choices[0].text
+    response = response.choices[0].text
 
     # The result should be 0, 1, and 2
-    print(drug_class)
+    #print(drug_class)
+
+    try:
+        print(drug_name + " is used for " + class_map[int(response)] )
+    except:
+        print("I don't know what " + drug_name + " is used for")
+    print()
