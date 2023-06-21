9f8da7a2510ccd5748501cd5de077be53f5f7b17
martin-creator <martinlubowa@outlook.com>
2023-06-21 23:41:48 +0300

    Successfully created text for drug classsification model

--- test_drug_classification.py
+++ test_drug_classification.py
@@ -1,14 +1,45 @@
 import os
+from urllib import response
 import openai
 
+
 def init_api():
+    ''' Load API key from .env file'''
     with open(".env") as env:
         for line in env:
             key, value = line.strip().split("=")
             os.environ[key] = value
 
+    openai.api_key = os.environ["API_KEY"]
+    openai.organization = os.environ["ORG_ID"]
 
-openai.api_key = os.environ.get("API_KEY")
-openai.organization = os.environ.get("ORG_ID")
 
 init_api()
+
+
+# configure the model ID.
+model = "curie:ft-personal-2023-06-21-19-07-39"
+
+# Let's use a drug from each class
+drugs = [
+    "A CN Gel(Topical) 20gmA CN Soap 75gm",  # Class 0
+    "Addnok Tablet 20'S",  # Class 1
+    "ABICET M Tablet 10's",  # Class
+]
+
+# Returns a drug class for each drug
+for drug_name in drugs:
+    prompt = "Drug: {}\nMalady:".format(drug_name)
+
+    response = openai.Completion.create(
+        model=model,
+        prompt=prompt,
+        temperature=1,
+        max_tokens=1,
+    )
+
+    # Print the generated text
+    drug_class = response.choices[0].text
+
+    # The result should be 0, 1, and 2
+    print(drug_class)
