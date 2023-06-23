105e20d843b42fbcff6eda2c5fb0b59938f01b49
martin-creator <martinlubowa@outlook.com>
2023-06-23 11:55:55 +0300

    Updated frontend for bot

--- conversationalapp/client/src/views/AboutView.vue
+++ conversationalapp/client/src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Welcome to our medical chatbot! Our chatbot is designed to provide information about various drug names. It utilizes a comprehensive database to give you accurate and up-to-date details about different medications.
+    </p>
+    <p>
+      With our chatbot, you can easily inquire about drug names, their uses, potential side effects, dosage instructions, and more. Whether you need information for personal use or professional reference, our chatbot is here to assist you.
+    </p>
+    <p>
+      We understand the importance of reliable and trustworthy drug information, so our chatbot's database is regularly updated by medical professionals and experts in the field. You can rely on the information provided by our chatbot to make informed decisions about medications.
+    </p>
+    <p>
+      Please keep in mind that while our chatbot provides valuable insights, it is not a substitute for professional medical advice. Always consult with a healthcare professional or pharmacist for personalized guidance and recommendations.
+    </p>
+    <p>
+      If you have any questions or need assistance, feel free to interact with our chatbot. We're here to help you!
+    </p>
   </div>
 </template>
--- conversationalapp/client/src/views/HomeView.vue
+++ conversationalapp/client/src/views/HomeView.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Medical Drug Assistant" />
+    <div v-if="messages.length">
+      <div v-for="message in messages" :key="message.id">
+        <strong>{{ message.author }}:</strong> {{ message.text }}
+      </div>
+    </div>
+
+    <form @submit.prevent="sendMessage">
+      <input type="text" v-model="newMessage" placeholder="Type your message" />
+      <button type="submit">Send</button>
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+
+  data(){
+    return{
+      messages: [
+        {id: 1, author: "AI", text: "Hello, how can I help you?"},
+      ], 
+      newMessage: "",
+    };
+  },
+
+  methods: {
+    sendMessage(){
+      if(this.newMessage.trim() == ""){
+        return;
+      }
+      this.messages.push({
+        id: this.messages.length + 1,
+        author: "Human",
+        text: this.newMessage.trim(),
+      });
+
+      const messageText = this.newMessage.trim();
+
+      axios.get(`http://127.0.0.1:5000/?m=${encodeURI(messageText)}`)
+      .then(response => {
+        console.log(response);
+        const message = {
+          id: this.messages.length + 1,
+          author: "AI",
+          text: response.data.m
+        };
+        this.messages.push(message);
+      })
+      .catch(error => {
+        console.log(error);
+        this.messages.push({
+            id: this.messages.length + 1,
+            author: "AI",
+            text: "Sorry, I don't understand",
+        });
+      });
+
+      this.newMessage = "";
+    },
+  },
+};
 </script>
